d211d0ce44dabbb4031f99ebc872a1ebd58fe52b
Maks-Budiukin <106489459+Maks-Budiukin@users.noreply.github.com>
2022-11-08 20:20:06 +0000

    Deploying to gh-pages from @ Maks-Budiukin/goit-js-hw-10@e11d5e4264e4b380239d5a9af5f0a89559e92e7a 🚀

--- 01-color-switcher.98550a22.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.setAttribute("disabled","disabled"),n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(n)}));var n=0}();
-//# sourceMappingURL=01-color-switcher.98550a22.js.map
--- 01-color-switcher.c3b1f514.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.setAttribute("disabled","disabled"),n=setInterval((function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(n)}));let n=0;
-//# sourceMappingURL=01-color-switcher.c3b1f514.js.map

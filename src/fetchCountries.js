d03d82acae2a4b4bba9fc746954b99212f8dce51
Maks-Budiukin <106489459+Maks-Budiukin@users.noreply.github.com>
2022-11-09 03:36:08 +0200

    cleared consolelog

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -38,7 +38,7 @@ export function fetchCountries(name) {
         .catch((error) => {
             
                 Notify.failure('Oops, there is no country with that name');
-            console.log(error);
+            
     })
 }
 

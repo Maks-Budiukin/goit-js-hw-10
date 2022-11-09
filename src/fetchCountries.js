11100fe6aab08897de2a787ae890436420c6b1dc
Maks-Budiukin <106489459+Maks-Budiukin@users.noreply.github.com>
2022-11-09 03:17:11 +0200

    completed whole task

---
+++ src/fetchCountries.js
@@ -0,0 +1,44 @@
+import { Notify } from "notiflix";
+
+const listEl = document.querySelector('.country-list');
+const infoEl = document.querySelector('.country-info');
+
+export function fetchCountries(name) {
+    fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+        .then(response => response.json())
+        .then(data => {
+            countries = data;
+
+            for (let { name: {official: countryName}, capital, population, flags: {svg: flag}, languages } of countries) {
+                    if (countries.length > 1 && countries.length < 11) {
+                        listEl.insertAdjacentHTML('beforeend', `
+                    <li>
+                    <img src="${flag}", width="70"></img>
+                    <span class="country-info__entry">${countryName}</span>
+                    </li>`);
+                    } else if (countries.length === 1) {
+                        infoEl.insertAdjacentHTML('beforeend', `
+                        <div class="country-info__name">
+                        <img src="${flag}", width="70"></img>
+                        <span class="country-info__entry">${countryName}</span>
+                        </div>
+                        <p><span class="country-info__entry">Capital:</span> ${capital}</p>
+                        <p><span class="country-info__entry">Population:</span> ${population}</p>
+                        <p><span class="country-info__entry">Languages:</span> ${Object.values(languages)}</p>
+                        `)
+                        
+                    } else {
+                        Notify.info("Too many matches found. Please enter a more specific name.");
+                        return;
+                    }
+                
+            }
+            // countries.map(country => listEl.insertAdjacentHTML('beforeend', `<img src="${country.flags.svg}", width="250"></img>`))
+        })
+        .catch(() => {
+            
+                Notify.failure('Oops, there is no country with that name');
+            
+    })
+}
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
+let debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+
+const listEl = document.querySelector('.country-list');
+const infoEl = document.querySelector('.country-info');
+const inputEl = document.querySelector('#search-box');
+
+inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(event) {
+    
+    listEl.innerHTML = "";
+    infoEl.innerHTML = "";
+
+    const inputVal = event.target.value.trim();
+
+    if(inputVal !== "") {
+        fetchCountries(inputVal);
+    }
+}
+
+
